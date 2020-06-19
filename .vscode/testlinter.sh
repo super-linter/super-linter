9bce2fecb9052370d918bed33b16ea40583a8803
Justin Grote <jgrote@allieddigital.net>
2020-06-19 17:32:09 +0000

    Minor refinements

--- .vscode/testlinter.sh
+++ .vscode/testlinter.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#At this point you can use the debug console to add export GITHUB_WORKSPACE=/path to test only a specific folder
+#You can also use ln -s /path /tmp/lint as an alternative
 export RUN_LOCAL=true
 /workspaces/super-linter/lib/linter.sh
