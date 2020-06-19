5064b0c67aa4fb981fe4693d573ce8258ad75af4
Justin Grote <jgrote@allieddigital.net>
2020-06-19 17:48:39 +0000

    Fix: Default Linter to automation tests

--- .vscode/testlinter.sh
+++ .vscode/testlinter.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #At this point you can use the debug console to add export GITHUB_WORKSPACE=/path to test only a specific folder
 #You can also use ln -s /path /tmp/lint as an alternative
+#If you do neither, this will default to running against the test automation files
+tmppath=/tmp/lint
+if [ ! -L tmppath ]; then
+  ln -s $PWD/.automation/test $tmppath
+fi
+
 export RUN_LOCAL=true
-/workspaces/super-linter/lib/linter.sh
+source $PWD/lib/linter.sh
