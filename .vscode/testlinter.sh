172e29dad3e4d7768590e191201522ece5b384f9
Gabo <gabo.fdc@gmail.com>
2020-06-25 10:03:39 -0500

    Fix bash lint errors

--- .vscode/testlinter.sh
+++ .vscode/testlinter.sh
@@ -4,8 +4,9 @@
 #If you do neither, this will default to running against the test automation files
 tmppath=/tmp/lint
 if [ ! -L $tmppath ]; then
-  ln -s $PWD/.automation/test $tmppath
+  ln -s "$PWD"/.automation/test $tmppath
 fi
 
 export RUN_LOCAL=true
-source $PWD/lib/linter.sh
+# shellcheck source=lib/linter.sh
+source "$PWD"/lib/linter.sh
