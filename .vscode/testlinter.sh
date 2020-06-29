3975731d07ad4e849c08aa2c8bbf8d1fbb2f56cf
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-06-29 09:08:04 -0500

    cleanup

--- .vscode/testlinter.sh
+++ .vscode/testlinter.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
-#At this point you can use the debug console to add export GITHUB_WORKSPACE=/path to test only a specific folder
-#You can also use ln -s /path /tmp/lint as an alternative
-#If you do neither, this will default to running against the test automation files
-tmppath=/tmp/lint
-if [ ! -L $tmppath ]; then
-  ln -s "$PWD"/.automation/test $tmppath
+#!/usr/bin/env bash
+
+# At this point you can use the debug console to add export GITHUB_WORKSPACE=/path to test only a specific folder
+# You can also use ln -s /path /tmp/lint as an alternative
+# If you do neither, this will default to running against the test automation files
+
+###########
+# GLOBALS #
+###########
+CODE_PATH='/tmp/lint' # Path to code base
+
+##################
+# Check the path #
+##################
+if [ ! -L $CODE_PATH ]; then
+  # Create symbolic link
+  ln -s "$PWD"/.automation/test $CODE_PATH
 fi
 
+#########################
+# Export to run locally #
+#########################
 export RUN_LOCAL=true
-# shellcheck source=lib/linter.sh
+
+# # shellcheck disable=SC1091
 source "$PWD"/lib/linter.sh
