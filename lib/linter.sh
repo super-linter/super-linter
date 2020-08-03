f9e2e5a74a8dc3ebc5683e8c4045d1489964809e
sirReeall <35725129+sirReeall@users.noreply.github.com>
2020-08-03 16:28:07 +0100

    hardcoded template file
    Might need to figure out how to change this at a later date

--- lib/linter.sh
+++ lib/linter.sh
@@ -1373,7 +1373,7 @@ if [ "$VALIDATE_JAVA" == "true" ]; then
   # Lint the JAVA files #
   #######################
   # LintCodebase "FILE_TYPE" "LINTER_NAME" "LINTER_CMD" "FILE_TYPES_REGEX" "FILE_ARRAY"
-  LintCodebase "JAVA" "checkstyle" "java -jar /usr/bin/checksytle.jar" ".*\.\(java\)\$" "${FILE_ARRAY_JAVA[@]}"
+  LintCodebase "JAVA" "checkstyle" "java -jar /usr/bin/checksytle.jar -c sun_checks.xml" ".*\.\(java\)\$" "${FILE_ARRAY_JAVA[@]}"
 fi
 
 ##################
