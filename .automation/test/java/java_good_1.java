621244f8dfe868eb042d7dcc8eb3f72b04215700
Thomas Hughes <iamhughes@github.com>
2020-08-05 17:04:13 -0500

    fix linter errors

--- .automation/test/java/java_good_1.java
+++ .automation/test/java/java_good_1.java
@@ -1,13 +1,13 @@
 package com.superlinter.JavaGood;
 /* This is a simple Java program.
-   FileName : "HelloWorld.java". */
+   FileName : "HelloWorld.java" */
 
 /**
  * Represents a good Java file
 */
-class JavaGood {
-    // Prints "Hello, World" to the terminal window.
-    public static void main(final String[] args) {
+public static class JavaGood {
+    // Prints "Hello, World" to the terminal window
+    private void JavaGood() {
         System.out.println("Hello, World");
     }
 }
