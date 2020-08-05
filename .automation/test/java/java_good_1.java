4e725cba042515ecd7431ade7784103f4d793a4e
Thomas Hughes <iamhughes@github.com>
2020-08-05 16:14:13 -0500

    Fix good java file?

--- .automation/test/java/java_good_1.java
+++ .automation/test/java/java_good_1.java
@@ -1,11 +1,11 @@
 /* This is a simple Java program.
    FileName : "HelloWorld.java". */
 
-package com.super-linter.JavaGood
+package com.superlinter.JavaGood
 
 public class JavaGood {
     // Prints "Hello, World" to the terminal window.
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello, World");
     }
 }
