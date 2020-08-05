3472359b2b03d92061c78a9107c80e9d65177dd7
Thomas Hughes <iamhughes@github.com>
2020-08-05 16:04:04 -0500

    Fix good java file?

--- .automation/test/java/java_good_1.java
+++ .automation/test/java/java_good_1.java
@@ -1,11 +1,11 @@
 /* This is a simple Java program.
    FileName : "HelloWorld.java". */
-class java_good
-{
-    // Your program begins with a call to main().
+
+package com.super-linter.JavaGood
+
+public class JavaGood {
     // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
         System.out.println("Hello, World");
     }
 }
