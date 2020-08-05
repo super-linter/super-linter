a6bdf858f29db33deb26f3682e68694b6be0494e
Thomas Hughes <iamhughes@github.com>
2020-08-05 17:39:49 -0500

    fix javadoc error

--- .automation/test/java/java_good_1.java
+++ .automation/test/java/java_good_1.java
@@ -3,11 +3,11 @@ package com.superlinter.JavaGood;
    FileName : "HelloWorld.java" */
 
 /**
- * Represents a good Java file
+ * Represents a good Java file.
 */
 public static class JavaGood {
-    // Prints "Hello, World" to the terminal window
-    private void JavaGood() {
+    // Prints "Hello, World" to the terminal window.
+    private void helloWorld() {
         System.out.println("Hello, World");
     }
 }
