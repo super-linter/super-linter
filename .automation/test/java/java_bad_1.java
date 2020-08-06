680a1e93f5b5ad8297ae40343ead829dfefc8e31
Lukas Gravley <admiralawkbar@github.com>
2020-08-06 08:24:02 -0500

    make compile

--- .automation/test/java/java_bad_1.java
+++ .automation/test/java/java_bad_1.java
@@ -3,7 +3,6 @@ class java_bad_1 // first letter of classname is not capitalized
 
     public static void main(String args[])
     {
-
-        System.out.println("Hello, World)
+        System.out.println("Hello, World")
     }
 }
