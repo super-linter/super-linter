2a162c85ab55f54eb7fb2963445ff60ac723c155
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-08-07 09:09:40 -0500

    saved the world

--- .automation/clean-code-base-for-tests.sh
+++ .automation/clean-code-base-for-tests.sh
@@ -192,6 +192,66 @@ RenameTestFolder() {
   fi
 }
 ################################################################################
+#### Function CleanPowershell ##################################################
+CleanPowershell() {
+  # Need to remove the .psd1 templates as they are formally parsed,
+  # and will fail with missing modules
+
+  info "-------------------------------------------------------"
+  info "Finding powershell template files... and removing them..."
+
+  ##################
+  # Find the files #
+  ##################
+  mapfile -t FIND_CMD < <(cd "${GITHUB_WORKSPACE}" || exit 1 ; find "${GITHUB_WORKSPACE}" -type f -name "*.psd1" 2>&1)
+
+  #######################
+  # Load the error code #
+  #######################
+  ERROR_CODE=$?
+
+  ##############################
+  # Check the shell for errors #
+  ##############################
+  if [ $ERROR_CODE -ne 0 ]; then
+    error "ERROR! failed to get list of all file for *.psd1!"
+    fatal "ERROR:[${FIND_CMD[*]}]"
+  fi
+
+  ############################################################
+  # Get the directory and validate it came from tests folder #
+  ############################################################
+  for FILE in "${FIND_CMD[@]}"; do
+    #####################
+    # Get the directory #
+    #####################
+    FILE_DIR=$(dirname "$FILE" 2>&1)
+
+    ##################################
+    # Check if from the tests folder #
+    ##################################
+    if [[ $FILE_DIR == **"TEMPLATES"** ]]; then
+      ################################
+      # Its a test, we can delete it #
+      ################################
+      REMOVE_FILE_CMD=$(cd "${GITHUB_WORKSPACE}" || exit 1; rm -f "$FILE" 2>&1)
+
+      #######################
+      # Load the error code #
+      #######################
+      ERROR_CODE=$?
+
+      ##############################
+      # Check the shell for errors #
+      ##############################
+      if [ $ERROR_CODE -ne 0 ]; then
+        error "ERROR! failed to remove file:[${FILE}]!"
+        fatal "ERROR:[${REMOVE_FILE_CMD[*]}]"
+      fi
+    fi
+  done
+}
+################################################################################
 ################################## MAIN ########################################
 ################################################################################
 
@@ -219,3 +279,8 @@ CleanSHAFolder
 # Re Name folder #
 ##################
 RenameTestFolder
+
+##############################
+# Clean Powershell templates #
+##############################
+CleanPowershell
