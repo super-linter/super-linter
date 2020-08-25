2ff9be6197e1b550cf114090dacd187b98b6fd60
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-08-25 14:05:42 -0500

    adding testes

--- .automation/test/csharp/csharp_bad_01.cs
+++ .automation/test/csharp/csharp_bad_01.cs
@@ -1,9 +1,12 @@
-/* Bad */
-/* Multi-line */
-/* Comment */
-.selector-3[type="text"] {
-    background: linear-gradient(#FFFFFF, rgba(0, 0, 0, 0.8));
-    box-sizing: border-box;
-    display: block;
-    color: #AAAAAA;
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Hello World);
+    }
+  }
 }
--- .automation/test/csharp/csharp_good_01.cs
+++ .automation/test/csharp/csharp_good_01.cs
@@ -1,22 +1,12 @@
-/**
- * Multi-line comment
- */
+using System;
 
-.selector-1,
-.selector-2,
-.selector-3[type="text"] {
-  background: linear-gradient(#fff, rgba(0, 0, 0, 0.8));
-  box-sizing: border-box;
-  display: block;
-  color: #333;
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
 }
-
-.selector-a,
-.selector-b:not(:first-child) {
-  padding: 10px !important;
-  top: calc(calc(1em * 2) / 3);
-}
-
-.selector-x { width: 10%; }
-.selector-y { width: 20%; }
-.selector-z { width: 30%; }
