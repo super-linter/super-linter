74160885884ae86e816df8e9c17e77bb513be0cf
Eddy Nakamura <ednakamu@microsoft.com>
2020-08-25 16:52:52 -0300

    updating spaces to be correct

--- .automation/test/csharp/csharp_good_01.cs
+++ .automation/test/csharp/csharp_good_01.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace HelloWorld
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
-  }
 }
