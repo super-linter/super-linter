add83739a71b9e3e2b6d251f59822d683a3a8e27
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-08-25 13:07:44 -0500

    adding test folders

---
+++ .automation/test/csharp/csharp_bad_01.cs
@@ -0,0 +1,9 @@
+/* Bad */
+/* Multi-line */
+/* Comment */
+.selector-3[type="text"] {
+    background: linear-gradient(#FFFFFF, rgba(0, 0, 0, 0.8));
+    box-sizing: border-box;
+    display: block;
+    color: #AAAAAA;
+}
