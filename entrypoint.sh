2a7532c89b24bbf7580e071506529e10bbfae8af
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-09-22 22:01:17 +0200

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,20 @@
 export PYTHONPATH=$PATHONPATH:`pwd`
 
 if [ "${TEST_CASE_RUN}" == "true" ] ; then
-  echo "Running test cases"
+  echo "RUNNING TEST CASES"
+  echo ""
+  echo "CURRENT FOLDER CONTENT"
+  echo ""
   ls -1
   if [ -d "/tmp/lint" ]; then
+      echo "CONTENT OF /tmp/lint"
+      echo ""
       ls "/tmp/lint" -1
+  fi
+  if [ -d "/action" ]; then
+      echo "CONTENT OF /action"
+      echo ""
+      ls "/action" -1
   fi
   python -m superlinter.test
 else
