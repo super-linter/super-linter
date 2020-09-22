b83716f218b5e4810778e0bed3ae500b0dac2260
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-09-22 17:31:46 +0200

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 export PYTHONPATH=$PATHONPATH:`pwd`
 
+ls -1
+
+if [ -d "/tmp/lint" ]
+then
+    ls "/tmp/lint" -1
+
 if [ "${TEST_CASE_RUN}" == "true" ]; then
   python -m superlinter.test
 else
