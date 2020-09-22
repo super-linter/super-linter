4ea40b4a9459b2817e2ec95adb7811dc7c88a319
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-09-22 15:54:10 +0200

    Test helper

--- superlinter/tests/test_superlinter/helpers/utilstest.py
+++ superlinter/tests/test_superlinter/helpers/utilstest.py
@@ -4,7 +4,7 @@ import os
 def linter_test_setup():
     root_dir = '/action' if os.path.exists('/action') else os.path.dirname(
         os.path.abspath(__file__)) + '/../../../..'
-    os.environ["LINTER_RULES_PATH"] = root_dir + '/.github/linters'
+    os.environ["LINTER_RULES_PATH"] = root_dir + '/lib/.automation' if os.path.exists(root_dir + '/lib/.automation') else root_dir + '/.github/linters'
     os.environ["GITHUB_WORKSPACE"] = root_dir + '/.automation/test' if os.path.exists(root_dir + '/.automation/test') else '/tmp/lint'
 
 
