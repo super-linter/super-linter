213fa75c34cec4385fec3360f6e3e8b4a9f27c0c
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-09-22 21:29:06 +0200

    Use absolute paths for tests

--- superlinter/tests/test_superlinter/helpers/utilstest.py
+++ superlinter/tests/test_superlinter/helpers/utilstest.py
@@ -8,9 +8,9 @@ from superlinter import SuperLinter
 def linter_test_setup():
     root_dir = '/action' if os.path.exists('/action') else os.path.dirname(
         os.path.abspath(__file__)) + '/../../../..'
-    os.environ["LINTER_RULES_PATH"] = root_dir + '/lib/.automation' if os.path.exists(
+    os.environ["LINTER_RULES_PATH"] = os.path.realpath(root_dir + '/lib/.automation') if os.path.exists(
         root_dir + '/lib/.automation') else root_dir + '/.github/linters'
-    os.environ["GITHUB_WORKSPACE"] = root_dir + '/.automation/test' if os.path.exists(
+    os.environ["GITHUB_WORKSPACE"] = os.path.realpath(root_dir + '/.automation/test') if os.path.exists(
         root_dir + '/.automation/test') else '/tmp/lint'
 
 
