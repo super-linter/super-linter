c164fd1cca71f41d8f89a549c2f1688266feb384
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2020-09-30 23:38:09 +0200

    Add JSX linter (#16)

---
+++ .automation/test/jsx/jsx_bad_1.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+
+import './Sample.less';
+import
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+};
+
+export default class Sample extends Component {
+  state = {
+    file: './sample.pdf', yooooo
+    numPages: null,
+  }
+
+  onFileChange = (event) => {
+    this.setState({
+      file: event.target.files[0],
+    });
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  render() {
+    const { file, numPages } = this.state;
+
+    return (
+      <div className="Example">
+        <header>
+          <h1>react-pdf sample page</h1>
+        </header>
+        <div className="Example__container">
+          <div className="Example__container__load">
+            <label htmlFor="file">Load from file:</label>
+            {' '}
+            <input
+              onChange={this.onFileChange}
+              type="file"
+            />
+          </div>
+          <div className="Example__container__document">
+            <Document
+              file={file}
+              onLoadSuccess={this.onDocumentLoadSuccess}
+              options={options}
+            >
+              {
+                Array.from(
+                  new Array(numPages),
+                  (el, index) => (
+                    <Page
+                      key={`page_${index + 1}`}
+                      pageNumber={index + 1}
+                    />
+                  ),
+                )
+              }
+            </Document>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
