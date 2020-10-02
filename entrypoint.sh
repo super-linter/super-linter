90640699e93a4b537c9f75a0c8ddc1d95ef4ca27
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2020-10-02 12:10:17 +0200

    Add r linter (#27)
    * Add R Linter
    
    * Add R Linter
    
    * Fix entrypoint
    
    * Fix get R version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ PYTHONPATH=$PYTHONPATH:$(pwd)
 export PYTHONPATH
 
 if [ "${TEST_CASE_RUN}" == "true" ] ; then
+  # Test cases run
   echo "RUNNING TEST CASES"
   echo ""
   echo "[CURRENT FOLDER CONTENT]"
@@ -18,8 +19,21 @@ if [ "${TEST_CASE_RUN}" == "true" ] ; then
       ls "/action" -a -1
       echo ""
   fi
+  # Run pytest
   pytest --cov=superlinter --cov-report=xml superlinter/
+  PYTEST_STATUS=$?
+  echo Pytest exited $PYTEST_STATUS
+  # Manage return code
+  if [ $PYTEST_STATUS -eq 0 ]; then
+    echo "Successfully executed Pytest"
+  else
+    echo "Error(s) found by Pytest"
+    exit 1
+  fi
+  # Upload to codecov.io
   bash <(curl -s https://codecov.io/bash)
+
 else
+  # Normal run
   python -m superlinter.run
 fi
---
+++ superlinter/linters/RLinter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Use lintr to lint R files
+https://github.com/jimhester/lintr
+"""
+import os
+from shutil import copyfile
+
+from superlinter import LinterTemplate
+
+
+class RLinter(LinterTemplate):
+    language = "R"
+    linter_name = "lintr"
+    linter_url = "https://github.com/jimhester/lintr"
+    config_file_name = '.lintr'
+    file_extensions = ['.r', '.R', '.Rmd', 'RMD']
+
+    # Build the CLI command to call to lint a file
+    def build_lint_command(self, file):
+        # lintr requires .lintr in folder: copy it there if necessary
+        dir_name = os.path.dirname(file)
+        if not os.path.exists(dir_name + os.path.sep + self.config_file_name):
+            copyfile(self.config_file, dir_name + os.path.sep + self.config_file_name)
+        # Build command in R format
+        r_commands = [f"errors <- lintr::lint('{file}');",
+                      "print(errors);",
+                      "quit(save = 'no', status = if (length(errors) > 0) 1 else 0)"]
+        # Build shell command
+        cmd = ["R",
+               "--slave",
+               "-e", "".join(r_commands)]
+        return cmd
+
+    # Build the CLI command to request lintr version
+    def build_version_command(self):
+        # Build command in R format
+        r_commands = ["packageVersion(\"lintr\");"]
+        # Build shell command
+        cmd = ["R",
+               "--slave",
+               "-e", "".join(r_commands)]
+        return cmd
