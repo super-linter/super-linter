0daf3c03e70cebb28f2c987b88402b3f9a6ce5b8
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-10-08 18:51:25 +0200

    Dotnet fix

---
+++ superlinter/linters/CSharpDotnetFormatLinter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Use dotnet-format to lint CSharp files
+"""
+
+import os.path
+
+from superlinter import Linter
+
+
+class CSharpDotnetFormatLinter(Linter):
+
+    # Build the CLI command to call to lint a file
+    def build_lint_command(self, file):
+        commands = [f'cd "{os.path.dirname(file)}" || exit 1',
+                    " ".join(super().build_lint_command(file)) + ' | tee /dev/tty2 2>&1',
+                    'exit "${PIPESTATUS[0]}"']
+        return " && ".join(commands)
