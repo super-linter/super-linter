7a775854e7ca21a5649029ee0c2bb016bb9101fc
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-10-13 13:46:46 -0500

    cleaner

--- lib/linter.sh
+++ lib/linter.sh
@@ -654,7 +654,7 @@ DetectKubernetesFile() {
   FILE="${1}" # File that we need to validate
   debug "Checking if ${FILE} is a Kubernetes descriptor..."
 
-  if grep -v 'kustomize.config.k8s.io' "${FILE}" | grep -v tekton "${FILE}" | grep -q -E '(apiVersion):'; then
+  if grep -v 'kustomize.config.k8s.io' "${FILE}" | grep -v tekton | grep -q -E '(apiVersion):'; then
     debug "${FILE} is a Kubernetes descriptor"
     return 0
   fi
