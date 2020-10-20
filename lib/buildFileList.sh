dd741a94144007131618e7ddc28cba8f8c46188c
zuphzuph <zuphzuph@users.noreply.github.com>
2020-10-19 21:55:01 -0400

    Update buildFileList.sh

--- lib/buildFileList.sh
+++ lib/buildFileList.sh
@@ -470,6 +470,7 @@ function BuildFileList() {
       ################################
       FILE_ARRAY_TERRAFORM+=("${FILE}")
       FILE_ARRAY_TERRAFORM_TERRASCAN+=("${FILE}")
+      FILE_ARRAY_TFSEC+=("${FILE}")
 
     ############################
     # Get the Terragrunt files #
