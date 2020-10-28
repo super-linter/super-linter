22dda4056236d718ee48635b0739b29aec179f39
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-10-28 10:50:02 -0500

    fix tests

--- .automation/test/python_isort/python_good_1.py
+++ .automation/test/python_isort/python_good_1.py
@@ -4,10 +4,8 @@ import os
 import sys
 
 from my_lib import Object, Object2, Object3
-
-from third_party import (lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8,
-                         lib9, lib10, lib11, lib12, lib13, lib14, lib15)
-
+from third_party import (lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9,
+                         lib10, lib11, lib12, lib13, lib14, lib15)
 
 print("Hey")
 print("yo")
