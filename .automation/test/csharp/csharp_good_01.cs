e1a83d9f70c9110139ccb3219860ea486d2f55e6
Marco Ferrari <ferrari.marco@gmail.com>
2020-11-06 22:13:57 +0100

    Fix C# good test

--- .automation/test/csharp/csharp_good_01.cs
+++ .automation/test/csharp/csharp_good_01.cs
@@ -4,9 +4,9 @@ namespace HelloWorld
 {
   class Program
   {
-      static void Main(string[] args)
-      {
-          Console.WriteLine("Hello World!");
-      }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
   }
 }
