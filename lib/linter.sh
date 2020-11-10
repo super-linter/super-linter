c9c5f6b13cc9e935082ee2ef657db48f20707ef1
Lucas Gravley <29484535+admiralAwkbar@users.noreply.github.com>
2020-11-10 14:50:22 -0600

    Set to default true

--- lib/linter.sh
+++ lib/linter.sh
@@ -247,7 +247,7 @@ DEFAULT_TEST_CASE_RUN='false'                       # Flag to tell code to run o
 ###############################################################
 DEFAULT_DISABLE_ERRORS='false'                                  # Default to enabling errors
 export DEFAULT_DISABLE_ERRORS                                   # Workaround SC2034
-ERROR_ON_MISSING_EXEC_BIT="${ERROR_ON_MISSING_EXEC_BIT:-false}" # Default to report a warning if a shell script doesn't have the executable bit set to 1
+ERROR_ON_MISSING_EXEC_BIT="${ERROR_ON_MISSING_EXEC_BIT:-true}" # Default to report an error if a shell script doesn't have the executable bit set to 1
 export ERROR_ON_MISSING_EXEC_BIT
 RAW_FILE_ARRAY=()                   # Array of all files that were changed
 export RAW_FILE_ARRAY               # Workaround SC2034
