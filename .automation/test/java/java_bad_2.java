db5c6fa86b3bf254754c8af30ae5d7df29dffd3a
Thad Craft <thad.craft@kohls.com>
2020-12-03 17:15:44 -0500

    Enable @SuppressWarnings annotation support by default for Java checkstyle

---
+++ .automation/test/java/java_bad_2.java
@@ -0,0 +1,12 @@
+public class Application {
+
+  /**
+   * main.
+   *
+   * @param args
+   */
+  public static void main(final String[] args) {
+    System.out.println("hello");
+  }
+
+}
---
+++ .automation/test/java/java_good_2.java
@@ -0,0 +1,13 @@
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
+public class Application {
+
+  /**
+   * main.
+   *
+   * @param args
+   */
+  public static void main(final String[] args) {
+    System.out.println("hello");
+  }
+
+}
