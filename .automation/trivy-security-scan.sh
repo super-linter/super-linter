4195ef08be9c1923012e295db9fa601ad7351ca4
Admiral Awkbar <admiralawkbar@github.com>
2021-02-18 10:12:21 -0600

    only show high and critical

--- .automation/trivy-security-scan.sh
+++ .automation/trivy-security-scan.sh
@@ -32,7 +32,7 @@ RunScan() {
   echo ""
   echo "-------------------------------------------------------"
   echo " Running scan on local code base..."
-  RUN_CMD=$("${GITHUB_WORKSPACE}/trivy" fs --format template --template @"${GITHUB_WORKSPACE}/${TEMPLATE_NAME}" -o "${REPORT_NAME}" --exit-code 1 "${GITHUB_WORKSPACE}" 2>&1)
+  RUN_CMD=$("${GITHUB_WORKSPACE}/trivy" fs --format template --template @"${GITHUB_WORKSPACE}/${TEMPLATE_NAME}" -o "${REPORT_NAME}" --severity HIGH,CRITICAL --exit-code 1 "${GITHUB_WORKSPACE}" 2>&1)
 
   #######################
   # Load the error code #
