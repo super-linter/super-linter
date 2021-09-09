a3ad489783a32b7a8c1d3539cddefd2134834513
Tibo Delor <delor.thibault@gmail.com>
2021-09-10 04:03:41 +1000

    Match AWS States file using "States" JSON key (#1949)
    * Match AWS states file using "States" key
    
    Matching only on `"Resource": "arn` is too wide and will match also aws json policy files
    
    * Update detectFiles.sh
    
    * spacing
    
    Co-authored-by: Admiral Awkbar <admiralawkbar@github.com>

--- lib/functions/detectFiles.sh
+++ lib/functions/detectFiles.sh
@@ -203,7 +203,8 @@ DetectAWSStatesFIle() {
   ###############################
   # check if file has resources #
   ###############################
-  if grep -q '"Resource": *"arn"*' "${FILE}"; then
+  if grep -q '"Resource": *"arn' "${FILE}" &&
+    grep -q '"States"' "${FILE}"; then
     # Found it
     return 0
   fi
