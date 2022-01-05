43896510e0a3718d3e8ca92f4b80e293037c1d25
Colwyn Fritze-Moor <colwyn.fritze-moor@legitscript.com>
2022-01-05 11:54:55 -0800

    chore: move tests

--- .automation/test/terraform_tflint/good/terraform_tflint_good_1.tf
+++ .automation/test/terraform_tflint/good/terraform_tflint_good_1.tf
@@ -12,14 +12,3 @@ resource "aws_instance" "good" {
     encrypted = true
   }
 }
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  version = "2.11.1"
-
-  bucket = "test-bucket"
-}
-
-module "good_relative_reference" {
-  source = "../modules/ec2_instance"
-}
