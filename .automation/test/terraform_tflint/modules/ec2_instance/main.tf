6854aaca4c97cc795bd84c6875a962151b72af13
Colwyn Fritze-Moor <80352425+colwynlegitscript@users.noreply.github.com>
2022-01-06 09:04:10 -0800

    feat: add support for Terraform modules in tflint (#2297)
    * fix: support tflint relative module references
    
    * chore: add test for relative module imports
    
    * chore: add terraform binary
    
    * chore: move tests
    
    * chore: add newlines to tests
    
    * chore: add newlines to tests
    
    * refactor: move terraform get
    
    * refactor: put terraform get back where it was
    
    Co-authored-by: Lukas Gravley <admiralawkbar@github.com>

---
+++ .automation/test/terraform_tflint/modules/ec2_instance/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "good" {
+  ami                         = "ami-0ff8a91507f77f867"
+  instance_type               = "t2.small"
+  associate_public_ip_address = false
+
+  vpc_security_group_ids = ["sg-12345678901234567"]
+  metadata_options {
+    http_endpoint = "disabled"
+  }
+
+  ebs_block_device {
+    encrypted = true
+  }
+}
