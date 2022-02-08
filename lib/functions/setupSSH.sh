e027d679dcb9ab6cde42b6f48ca683dbcdcc48c3
Colwyn Fritze-Moor <colwyn.fritze-moor@legitscript.com>
2022-02-08 14:51:02 -0800

    refactor: allow github.com setup and update docs

--- lib/functions/setupSSH.sh
+++ lib/functions/setupSSH.sh
@@ -11,20 +11,22 @@ function SetupSshAgent() {
 }
 
 function SetupGithubComSshKeys() {
-  info "Adding github.com SSH keys"
-  # Fetched out of band from
-  # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
-  GITHUB_RSA_FINGERPRINT="SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8"
-  ssh-keyscan -t rsa github.com > /tmp/github.pub 2> /dev/null
-  if [[ "${NO_VERIFY_GITHUB_SSH_KEY}" ]]; then
-    warn "Skipping github.com key verification and adding without checking fingerprint"
-    mkdir -p ~/.ssh
-    cat /tmp/github.pub >> ~/.ssh/known_hosts
-  elif [[ "$(ssh-keygen -lf /tmp/github.pub)" == "2048 ${GITHUB_RSA_FINGERPRINT} github.com (RSA)" ]]; then
-    info "Successfully verified github.com key"
-    mkdir -p ~/.ssh
-    cat /tmp/github.pub >> ~/.ssh/known_hosts
-  else
-    error "Could not verify github.com key. SSH requests to github.com will likely fail."
+  if [[ -n "${SSH_KEY}" || "${SSH_SETUP_GITHUB}" == "true" ]]; then
+    info "Adding github.com SSH keys"
+    # Fetched out of band from
+    # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+    GITHUB_RSA_FINGERPRINT="SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8"
+    ssh-keyscan -t rsa github.com > /tmp/github.pub 2> /dev/null
+    if [[ "${SSH_INSECURE_NO_VERIFY_GITHUB_KEY}" == "true" ]]; then
+      warn "Skipping github.com key verification and adding without checking fingerprint"
+      mkdir -p ~/.ssh
+      cat /tmp/github.pub >> ~/.ssh/known_hosts
+    elif [[ "$(ssh-keygen -lf /tmp/github.pub)" == "2048 ${GITHUB_RSA_FINGERPRINT} github.com (RSA)" ]]; then
+      info "Successfully verified github.com key"
+      mkdir -p ~/.ssh
+      cat /tmp/github.pub >> ~/.ssh/known_hosts
+    else
+      error "Could not verify github.com key. SSH requests to github.com will likely fail."
+    fi
   fi
 }
