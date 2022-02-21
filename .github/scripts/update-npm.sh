86c82e43f742aeb38b66a6c3b343964ed59cb86d
Brett Logan <lindluni@github.com>
2022-02-21 10:50:54 -0500

    Add loggin

--- .github/scripts/update-npm.sh
+++ .github/scripts/update-npm.sh
@@ -7,18 +7,25 @@ npm i -g npm-check-updates
 pushd dependencies
 
 # Update all dependencies
+echo "Installing NPM dependencies..."
+npm install
+echo "Updating NPM dependencies..."
 ncu -u
+echo "Installing NPM dependencies..."
 npm install
 
 # Setup Git Config
+echo "Configuring Git..."
 git config --global user.email "noreply@github.com"
 git config --global user.name "Super-Linter Automation"
 
 # Push changes to remote
+echo "Pushing changes to remote..."
 git add .
 git commit -a -Ss -m "Update NPM dependencies"
 git checkout -b "npm_deps_${id}"
 git push origin "npm_deps_${id}"
 
 # Open pull request
+echo "Opening pull request..."
 gh pr create --title "Weekly NPM Updates" --body "Updates NPM dependencies" --base master --head "npm_deps_${id}"
