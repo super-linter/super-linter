ea7d5dc4ebff690b44e5e4f678c1bdeaf849adcc
Admiral Awkbar <admiralawkbar@github.com>
2022-04-19 10:29:02 -0700

    make a var

--- lib/functions/buildFileList.sh
+++ lib/functions/buildFileList.sh
@@ -84,7 +84,7 @@ function BuildFileList() {
     # Switch codebase back to the default branch to get a list of all files changed #
     #################################################################################
     SWITCH_CMD=$(
-      git -c "${GITHUB_WORKSPACE}" config --global --add safe.directory "/github/workspace"
+      git -c "${GITHUB_WORKSPACE}" config --global --add safe.directory "${GITHUB_WORKSPACE}"
       git -C "${GITHUB_WORKSPACE}" pull --quiet
       git -C "${GITHUB_WORKSPACE}" checkout "${DEFAULT_BRANCH}" 2>&1
     )
