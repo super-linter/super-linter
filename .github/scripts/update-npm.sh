554d229522e35d1ef899efa281021c153378576a
Brett Logan <lindluni@github.com>
2022-05-08 21:47:25 -0400

    Remove lockfile when updating deps

--- .github/scripts/update-npm.sh
+++ .github/scripts/update-npm.sh
@@ -11,6 +11,8 @@ echo "Installing NPM dependencies..."
 npm install
 echo "Updating NPM dependencies..."
 ncu -u
+echo "Removing existing lockfile..."
+rm -rf package-lock.json
 echo "Installing NPM dependencies..."
 npm install
 
