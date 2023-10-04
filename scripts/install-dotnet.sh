0c553bb34742113f72d5f2895046cd133eae458a
Vadim Hatsura <vadim.hatsura@gmail.com>
2023-10-04 22:48:03 +0200

    chore: remove dotnet-format installation from install-dotnet.sh
    dotnet format is a part of .NET 6 SDK and shouldn't be installed separately anymore

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -5,4 +5,3 @@ set -euo pipefail
 curl --retry 5 --retry-delay 5 -sLO https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --install-dir /usr/share/dotnet -channel STS -version latest
-/usr/share/dotnet/dotnet tool install --tool-path /usr/bin dotnet-format --version 5.0.211103
