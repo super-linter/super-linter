ad9aacefd3a784d9c2c9a03ee234d79b596ed0e5
Marco Ferrari <ferrari.marco@gmail.com>
2023-11-30 22:43:40 +0100

    Simplify validation script

--- .automation/validate-docker-labels.sh
+++ .automation/validate-docker-labels.sh
@@ -10,15 +10,11 @@ shift
 BUILD_VERSION="${1}"
 shift
 
-GetContainerImageLabel() {
-  echo "$(docker inspect --format "{{ index .Config.Labels \"${1}\" }}" "${2}")"
-}
-
 ValidateLabel() {
   local LABEL_KEY="$1"
   local CONTAINER_VALUE="$2"
 
-  LABEL="$(GetContainerImageLabel "${LABEL_KEY}" "${CONTAINER_IMAGE_ID}")"
+  LABEL="$(docker inspect --format "{{ index .Config.Labels \"${LABEL_KEY}\" }}" "${CONTAINER_IMAGE_ID}")"
 
   if [[ "${LABEL}" != "${CONTAINER_VALUE}" ]]; then
     echo "[ERROR] Invalid container image label: ${LABEL_KEY}: ${LABEL}. Expected: ${CONTAINER_VALUE}"
