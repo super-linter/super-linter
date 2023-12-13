7906450dce4f32b37999e0f0240403c92358f96c
Marco Ferrari <ferrari.marco@gmail.com>
2023-12-13 11:04:13 +0000

    Be more verbose when installing dart

--- scripts/install-dart-sdk.sh
+++ scripts/install-dart-sdk.sh
@@ -17,5 +17,8 @@ esac
 
 curl --retry 5 --retry-delay 5 -sO "https://storage.googleapis.com/dart-archive/channels/stable/release/${DART_VERSION}/sdk/dartsdk-linux-${target}-release.zip"
 unzip -q dartsdk-linux-${target}-release.zip
-chmod +x dart-sdk/bin/dart* && mv dart-sdk/bin/* /usr/bin/ && mv dart-sdk/lib/* /usr/lib/ && mv dart-sdk/include/* /usr/include/
-rm -r dart-sdk/ dartsdk-linux-${target}-release.zip
+chmod +x dart-sdk/bin/dart*
+mv -v dart-sdk/bin/* /usr/bin/
+mv -v dart-sdk/lib/* /usr/lib/
+mv -v dart-sdk/include/* /usr/include/
+rm -rv dart-sdk/ dartsdk-linux-${target}-release.zip
