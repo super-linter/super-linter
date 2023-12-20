34b1fbe27e74b1b4f8366a1e8264a4626f4cea1f
Marco Ferrari <ferrari.marco@gmail.com>
2023-12-20 07:01:54 +0000

    ci: don't validate dependabot commits
    Dependabot doesn't allow configuring the max commit message line length
    until https://github.com/dependabot/dependabot-core/issues/2445 is
    resolved, so we cannot validate Dependabot commits at this time.

--- .github/linters/commitlint.config.js
+++ .github/linters/commitlint.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  helpUrl: 'https://www.conventionalcommits.org/'
+  helpUrl: 'https://www.conventionalcommits.org/',
+  // We need this until https://github.com/dependabot/dependabot-core/issues/2445
+  // is resolved.
+  ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)]
 }
