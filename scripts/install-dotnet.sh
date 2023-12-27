5a175c2e27814a6f609fa7783993701d970bce21
Adam Ralph <adam@adamralph.com>
2023-12-27 09:26:05 +0000

    feat: install .NET LTS instead of STS (#5047)
    Switch to the LTS .NET release channel to allow .NET 8 to be installed. STS restricts to .NET 7.

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 curl --retry 5 --retry-delay 5 -sLO https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh --install-dir /usr/share/dotnet -channel STS -version latest
+./dotnet-install.sh --install-dir /usr/share/dotnet -channel LTS -version latest
