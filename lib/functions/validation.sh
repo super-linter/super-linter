137be5f44b418b3d5048093816c5a524995d63a1
Marco Ferrari <ferrari.marco@gmail.com>
2023-12-30 16:11:54 +0000

    fix: fail when validating as expected
    Fail when USE_FIND_ALGORITHM is true and VALIDATE_ALL_CODEBASE is false
    as expected.

--- lib/functions/validation.sh
+++ lib/functions/validation.sh
@@ -8,7 +8,7 @@ function GetValidationInfo() {
   info "Gathering user validation information..."
 
   if [[ "${USE_FIND_ALGORITHM}" == "true" ]] && [[ "${VALIDATE_ALL_CODEBASE}" == "false" ]]; then
-    fail "Setting USE_FIND_ALGORITHM to true and VALIDATE_ALL_CODEBASE to false is not supported because super-linter relies on Git to validate changed files."
+    fatal "Setting USE_FIND_ALGORITHM to true and VALIDATE_ALL_CODEBASE to false is not supported because super-linter relies on Git to validate changed files."
   fi
 
   ###########################################
