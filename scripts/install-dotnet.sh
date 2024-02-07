17c6333690a2994242eb0bf2ac1caf0fa8267521
Adam Ralph <adam@adamralph.com>
2024-02-07 12:14:39 +0100

    fix: use .NET version 8.0.101
    Instead of a floating LTS version, which appears to install 6.x

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 curl --retry 5 --retry-delay 5 -sLO https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh --install-dir /usr/share/dotnet -channel LTS -version latest
+./dotnet-install.sh --install-dir /usr/share/dotnet --version 8.0.101
 rm -rfv ./dotnet-install.sh
