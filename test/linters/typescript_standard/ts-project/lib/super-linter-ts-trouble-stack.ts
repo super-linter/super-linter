ee001cd4f9cdb1be8ad65acb226d6dc5711c4d65
Marco Ferrari <ferrari.marco@gmail.com>
2024-02-16 15:03:27 +0000

    fix: fix typescript linting
    Fix #4445

---
+++ test/linters/typescript_standard/ts-project/lib/super-linter-ts-trouble-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class SuperLinterTsTroubleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'SuperLinterTsTroubleQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/linters/typescript_standard/ts-project/test/super-linter-ts-trouble.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SuperLinterTsTrouble from '../lib/super-linter-ts-trouble-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/super-linter-ts-trouble-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SuperLinterTsTrouble.SuperLinterTsTroubleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
