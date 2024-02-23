761ccfbfd3078f4425120b7476707eb5065e060e
Chongyi Zheng <git@zcy.dev>
2024-02-22 19:12:41 -0500

    feat: support arm64

--- dependencies/python/build-venvs.sh
+++ dependencies/python/build-venvs.sh
@@ -8,11 +8,21 @@
 #####################
 set -euo pipefail
 
-apk add --no-cache --virtual .python-build-deps \
-  gcc \
-  linux-headers \
-  musl-dev \
+packages=(
+  gcc
+  linux-headers
+  musl-dev
   python3-dev
+)
+
+if [[ "${TARGETARCH}" != "amd64" ]]; then
+  # libffi-dev is required for building wheel for cffi,
+  # until https://github.com/python-cffi/cffi/issues/69 is merged
+  packages+=(libffi-dev)
+fi
+
+apk add --no-cache --virtual .python-build-deps \
+  "${packages[@]}"
 
 ############################
 # Create staging directory #
@@ -23,6 +33,14 @@ mkdir -p /venvs
 # Install basic libs to run installers #
 ########################################
 pip install virtualenv
+
+if [[ "${TARGETARCH}" != "amd64" ]]; then
+  # Install Rust compiler (required by checkov on arm64) #
+  # remove this once https://github.com/bridgecrewio/checkov/pull/6045 is merged
+  apk add --no-cache curl
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  export PATH=$PATH:$HOME/.cargo/bin
+fi
 
 #######################################################
 # Iterate through requirments.txt to install binaries #
---
+++ scripts/scalafmt-arm64.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "scalafmt is not supported on arm64"
