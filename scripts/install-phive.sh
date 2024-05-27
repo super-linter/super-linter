306b9c25a0b4a252acbda9871764ca9f490bb360
Marco Ferrari <ferrari.marco@gmail.com>
2024-05-27 10:01:05 +0000

    build: update php linters
    - Composer to 2.7.6
    - PHPCS to 3.10.1
    - PHPStan to 1.11.2
    - Psalm to 5.24.0

--- scripts/install-phive.sh
+++ scripts/install-phive.sh
@@ -8,7 +8,7 @@ apk add --no-cache --virtual .php-build-deps \
 # Install phive
 curl --retry 5 --retry-delay 5 -sLO https://phar.io/releases/phive.phar
 curl --retry 5 --retry-delay 5 -sLO https://phar.io/releases/phive.phar.asc
-gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys "0x9D8A98B29B2D5D79"
+gpg --keyserver hkps://keys.openpgp.org --recv-keys "0x9D8A98B29B2D5D79"
 gpg --verify phive.phar.asc phive.phar
 chmod +x phive.phar
 mv phive.phar /usr/local/bin/phive
@@ -16,7 +16,7 @@ rm phive.phar.asc
 
 # Install the PHARs listed in phive.xml
 phive --no-progress install \
-  --trust-gpg-keys 31C7E470E2138192,CF1A108D0E7AE720,8A03EA3B385DBAA1,12CE0F1D262429A5,5E6DDE998AB73B8E,51C67305FFC2E5C0,CBB3D576F2A0946F \
+  --trust-gpg-keys 31C7E470E2138192,CF1A108D0E7AE720,8A03EA3B385DBAA1,12CE0F1D262429A5,5E6DDE998AB73B8E,51C67305FFC2E5C0,CBB3D576F2A0946F,689DAD778FF08760E046228BA978220305CD5C32 \
   --target /usr/bin
 
 apk del --no-network --purge .php-build-deps
