a0a6967e92dd29df9338ef0ec2521b11f6006ea2
Vimal Vijayasekar <vimal.vijayasekar@wavicledata.com>
2025-05-20 22:47:34 +0530

    feat: Add vue files linting support

---
+++ test/linters/vue/vue_bad_1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="example">{{ msg }}
+    <section></section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello world!",
+    };
+  },
+};
+</script>
+
+<style>
+.example {
+  color: red;
+}
+</style>
+
+<custom1>
+  This could be e.g. documentation for the component.
+</custom1>
